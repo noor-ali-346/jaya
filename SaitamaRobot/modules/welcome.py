c8c964a01f534addede255551e668330467991cc
Eagle owner <73874131+noor-ali-346@users.noreply.github.com>
2020-12-31 03:08:39 +0500

    Update welcome.py

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -181,7 +181,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Devs
             elif new_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "Whoa! A member of the 🍁SOLO•GUILD🍁 just joined!",
+                    "Whoa! A member of the Eagle Association just joined!",
                     reply_to_message_id=reply,
                 )
                 continue
